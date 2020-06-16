5bfd43f0eb5e50f6b76d59f34455efc461410b64
Resha Puspita <reshapuspita734@gmail.com>
2020-06-16 15:46:05 +0700

    add files

---
+++ index.js
@@ -0,0 +1,7 @@
+const helloWorld = document.getElementById("helloW");
+console.log(helloWorld);
+
+helloWorld.remove();
+
+const helloName = document.getElementById("helloName");
+helloName.style.textAlign = "center";
